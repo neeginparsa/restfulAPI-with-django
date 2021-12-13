64e24aa1f8ca0ded397c8ee696d3ac3064700e1a
neeginparsa <neeginparsa2001@gmail.com>
2021-12-13 16:36:05 +0330

    use ListCreateAPIView &  RetrieveUpdateDestroyAPIView

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,7 @@ from . import views
 # URLConf
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
-    path('products/<int:id>/', views.ProductDetail.as_view()),
+    path('products/<int:pk>/', views.ProductDetail.as_view()),
     path('collections/', views.collectionlist.as_view()),
-    path('collections/<int:id>/', views.collectionDetail.as_view(), name='collection-detail'),
+    path('collections/<int:pk>/', views.collectionDetail.as_view(), name='collection-detail'),
 ]
